d9f2e9c5e87cf16c190cff701707873c3c91b0b4
szerem <szerem@go2.pl>
2024-08-23 23:55:54 +0200

    switchMap concatMap mergeMap

--- apm/src/app/app.component.ts
+++ apm/src/app/app.component.ts
@@ -1,6 +1,7 @@
 //import 'zone.js/dist/zone';  // Required for Stackblitz
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLinkActive, RouterLink, RouterOutlet } from '@angular/router';
+import { concatMap, delay, mergeMap, of, range, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'pm-root',
@@ -9,10 +10,54 @@ import { RouterLinkActive, RouterLink, RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   // Just enough here for the template to compile
   pageTitle = 'Acme Product Management';
 
   cartCount = 0;
 
+  ngOnInit(): void {
+
+    // sekwencyjnie
+    // jak sztafeta
+    range(0, 5).pipe(
+      // tap(x => console.log(`c ${x} begin`)),
+      concatMap(i => of(i).pipe(
+        // tap(x => console.log(`cc ${x} begin`)),
+        delay(this.randomDaley()),
+        // tap(x => console.log(`cc ${x} end`)),
+      )),
+      // tap(x => console.log(`c ${x} end`)),
+    ).subscribe(v => console.log('concatMap', v))
+
+    // rownolegle
+    // wyścig
+    range(11, 5).pipe(
+      // tap(x => console.log(`m ${x} begin`)),
+      mergeMap(i => of(i).pipe(
+        // tap(x => console.log(`mm ${x} begin`)),
+        delay(this.randomDaley()),
+        // tap(x => console.log(`mm ${x} end`)),
+      )),
+      // tap(x => console.log(`m ${x} end`)),
+    ).subscribe(v => console.log('mergeMap', v))
+
+    // po kolei i zwroci ostatni
+    // podmiana zawsze ostatni jest emitowany
+    range(21, 5).pipe(
+      // tap(x => console.log(`s ${x} begin`)),
+      switchMap(i => of(i).pipe(
+        // tap(x => console.log(`ss ${x} begin`)),
+        delay(this.randomDaley()),
+        // tap(x => console.log(`ss ${x} end`)),
+      )),
+      // tap(x => console.log(`s ${x} end`)),
+    ).subscribe(v => console.log('switchMap', v))
+
+  }
+
+
+  randomDaley() {
+    return Math.floor(Math.random() * 1000) + 500;
+  }
 }
--- apm/src/app/products/product.service.ts
+++ apm/src/app/products/product.service.ts
@@ -1,8 +1,10 @@
 import { Injectable, signal, inject } from '@angular/core';
-import { catchError, EMPTY, Observable, tap, throwError } from 'rxjs';
+import { catchError, concatMap, EMPTY, map, Observable, of, switchMap, tap, throwError } from 'rxjs';
 import { Product } from './product';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { HttpErrorService } from '../utilities/http-error.service';
+import { Review } from '../reviews/review';
+import { ReviewService } from '../reviews/review.service';
 
 
 @Injectable({
@@ -12,6 +14,7 @@ export class ProductService {
   private productsUrl = 'api/products';
 
   private http = inject(HttpClient);
+  private reviewService = inject(ReviewService);
   private errorService = inject(HttpErrorService);
 
   getProducts(): Observable<Product[]> {
@@ -23,10 +26,27 @@ export class ProductService {
 
   getProduct(id: number): Observable<Product> {
     const productsUrl = `${this.productsUrl}/${id}`;
-    return this.http.get<Product>(productsUrl).pipe(
-      tap(() => console.log('in http.get by id pipeline')),
-      catchError(err => this.handlerError(err))
-    );
+    return this.http.get<Product>(productsUrl)
+      .pipe(
+        // tap(_ => console.log('in http.get by id pipeline 1')),
+        switchMap(product => this.getProductWithReview(product)),
+        catchError(err => this.handlerError(err)),
+        // tap(_ => console.log('in http.get by id pipeline 2')),
+      );
+  }
+
+  getProductWithReview(product: Product): Observable<Product> {
+    if (product.hasReviews) {
+      return this.http.get<Review[]>(this.reviewService.getReviewUrl(product.id))
+        .pipe(
+          // tap(_ => console.log('in http.get with review pipeline 1')),
+          map(reviews => ({ ...product, reviews } as Product)),
+          catchError(err => this.handlerError(err)),
+          // tap(_ => console.log('in http.get with review pipeline 2')),
+        );
+    } else {
+      return of(product);
+    }
   }
 
   handlerError(err: HttpErrorResponse): Observable<never> {
