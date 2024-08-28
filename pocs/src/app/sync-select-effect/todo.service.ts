37395abd7d763812f9b8ef57e15f298a033d3a63
szerem <szerem@go2.pl>
2024-08-28 09:57:37 +0200

    sync-select-effect declarative mode

--- pocs/src/app/sync-select-effect/todo.service.ts
+++ pocs/src/app/sync-select-effect/todo.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { effect, inject, Injectable, signal } from '@angular/core';
-import { catchError, map, Observable, of, tap } from 'rxjs';
+import { inject, Injectable, signal } from '@angular/core';
+import { catchError, filter, map, Observable, of, switchMap, tap } from 'rxjs';
 import { setErrorMessage } from '../utility/errorHandling';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 
 @Injectable({
   providedIn: 'root'
@@ -12,20 +13,36 @@ export class TodoService {
 
   // Services
   private http = inject(HttpClient);
-  // private destroyRef = inject(DestroyRef);
 
-  todos = signal<Todo[]>([]);
+  // todos = signal<Todo[]>([]);
   errorMessage = signal('');
 
-  selectedMemberId = signal<number>(undefined!);
+  private selectedMemberId = signal<number>(undefined!);
+  private selectedMemberId$ = toObservable(this.selectedMemberId);
+
   setMemberId(memberId: number): void {
-    this.todos.set([]);
+    // this.todos.set([]);
     // this.getTodos(memberId).subscribe(this.todos.set);
     this.selectedMemberId.set(memberId);
   }
-  effectSelectedMemberId = effect(() => {
-      this.getTodos(this.selectedMemberId()).subscribe(this.todos.set);
-  });
+  // effectSelectedMemberId = effect(() => {
+  //     this.getTodos(this.selectedMemberId()).subscribe(this.todos.set);
+  // });
+
+  readonly todos$ = this.selectedMemberId$.pipe(
+    filter(Boolean),
+    switchMap(id => this.http.get<Todo[]>(`${this.todoUrl}?userId=${id}`)
+      .pipe(
+        tap(() => console.log(id)),
+        map(data => data.map(t => t.title.length > 20 ? ({ ...t, title: t.title.substring(0, 20) }) : t)),
+        catchError(err => {
+          this.errorMessage.set(setErrorMessage(err));
+          return of([])
+        })
+      )
+    )
+  );
+  todos = toSignal(this.todos$)
 
   private getTodos(id: number): Observable<Todo[]> {
     const todoUrl = `${this.todoUrl}?userId=${id}`;
@@ -33,7 +50,7 @@ export class TodoService {
     return this.http.get<Todo[]>(todoUrl).pipe(
       map(data => data.map(t => t.title.length > 20 ? ({ ...t, title: t.title.substring(0, 20) }) : t)),
       catchError(err => {
-        this.errorMessage.set(setErrorMessage(err)); 
+        this.errorMessage.set(setErrorMessage(err));
         return of([])
       })
     )
